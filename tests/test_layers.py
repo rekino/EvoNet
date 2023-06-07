a996821b5dcac01b0e7d6da7367b38c3b8ef5e53
Ramin Barati <ramin.barati@carnext.com>
2023-06-07 23:19:28 +0330

    add tests for reducer and imaginary part

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -2,7 +2,7 @@ import unittest
 import torch
 import numpy as np
 
-from src.EvoNet.layers.convolution import ComplexLinear, HarmonicConv2d
+from src.EvoNet.layers.convolution import ComplexLinear, HarmonicConv2d, Reducer
 
 class TestLayers(unittest.TestCase):
 
@@ -19,7 +19,7 @@ class TestLayers(unittest.TestCase):
 
     def test_harmonic_conv2d(self):
         layer = HarmonicConv2d((3, 3), 2, torch.ones(1, 1, 1, 1), 1)
-        out = layer(2*torch.rand(4, 1, 3, 3)-1).detach().numpy()
+        out = layer(torch.rand(4, 1, 3, 3)).detach().numpy()
 
         self.assertEqual(len(out.shape), 2)
         self.assertEqual(out.shape[0], 4)
@@ -27,3 +27,39 @@ class TestLayers(unittest.TestCase):
 
         self.assertFalse(np.any(np.isinf(out)))
         self.assertFalse(np.any(np.isnan(out)))
+
+        out = layer.conjugate(2*torch.rand(4, 1, 3, 3)-1).detach().numpy()
+
+        self.assertEqual(len(out.shape), 2)
+        self.assertEqual(out.shape[0], 4)
+        self.assertEqual(out.shape[1], 2)
+
+        self.assertFalse(np.any(np.isinf(out)))
+        self.assertFalse(np.any(np.isnan(out)))
+
+    def test_reducer(self):
+        layer = Reducer([
+            HarmonicConv2d((3, 3), 2, torch.ones(1, 1, 1, 1), 1),
+            HarmonicConv2d((3, 3), 2, torch.ones(1, 1, 1, 1), 1),
+        ])
+        out = layer(torch.rand(4, 1, 3, 3)).detach().numpy()
+
+        self.assertEqual(len(out.shape), 2)
+        self.assertEqual(out.shape[0], 4)
+        self.assertEqual(out.shape[1], 2)
+
+        self.assertFalse(np.any(np.isinf(out)))
+        self.assertFalse(np.any(np.isnan(out)))
+
+        out = layer.conjugate(2*torch.rand(4, 1, 3, 3)-1).detach().numpy()
+
+        self.assertEqual(len(out.shape), 2)
+        self.assertEqual(out.shape[0], 4)
+        self.assertEqual(out.shape[1], 2)
+
+        self.assertFalse(np.any(np.isinf(out)))
+        self.assertFalse(np.any(np.isnan(out)))
+
+
+
+
