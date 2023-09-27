fix the computation hyp0f1 value

src/EvoNet/functions/hypergeometric.py
@@ -6,7 +6,7 @@ class Hyp0F1(Function):
     @staticmethod
     def forward(ctx, b, z):
         # Forward pass using mpmath.hyp0f1
-        result = float(mpmath.hyp0f1(b, z))
+        result = float(mpmath.hyp0f1(b.item(), z.detach().item()))
         # Save input for backward pass
         ctx.save_for_backward(b, z)
         return torch.tensor(result, requires_grad=True)

tests/test_functions.py
@@ -8,7 +8,10 @@ from src.EvoNet.functions import Hyp0F1
 class TestFunctions(unittest.TestCase):
 
     def test_hyp0f1(self):
-        z = 1.0
-        res = Hyp0F1.apply(0.5, z)
+        z = torch.tensor(1.0, requires_grad=True)
+        b = torch.tensor(0.5, requires_grad=False)
+        res = Hyp0F1.apply(b, z)
 
-        self.assertAlmostEqual(res, np.cosh(2 * np.sqrt(z)))
+        self.assertAlmostEqual(res, np.cosh(2 * np.sqrt(z.detach())))
+
+        pass