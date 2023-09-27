fix and test hyp0f1 derivative

tests/test_functions.py
@@ -1,17 +1,23 @@
 import unittest
 import torch
-import numpy as np
-
+from torch.autograd import grad
 
 from src.EvoNet.functions import Hyp0F1
 
 class TestFunctions(unittest.TestCase):
 
-    def test_hyp0f1(self):
+    def test_hyp0f1_val(self):
         z = torch.tensor(1.0, requires_grad=True)
         b = torch.tensor(0.5, requires_grad=False)
         res = Hyp0F1.apply(b, z)
 
-        self.assertAlmostEqual(res, np.cosh(2 * np.sqrt(z.detach())))
+        self.assertAlmostEqual(res, torch.cosh(2 * torch.sqrt(z)))
+    
+    def test_hyp0f1_der(self):
+        z = torch.tensor(1.0, requires_grad=True)
+        b = torch.tensor(0.5, requires_grad=False)
+        res = Hyp0F1.apply(b, z)
 
-        pass
+        dz = grad(res, z)[0]
+
+        self.assertAlmostEqual(dz, torch.sinh(2 * torch.sqrt(z)) / torch.sqrt(z)) 