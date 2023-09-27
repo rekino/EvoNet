646feaac6e20bf0b646e573d73a50d9e71ce6d42
Ramin Barati <ramin.barati@carnext.com>
2023-09-27 12:53:26 +0330

    fix __round__ error in tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -10,16 +10,17 @@ class TestFunctions(unittest.TestCase):
     def test_hyp0f1_val(self):
         z = torch.tensor(1.0, requires_grad=True)
         b = torch.tensor(0.5, requires_grad=False)
-        res = Hyp0F1.apply(b, z)
+        res = Hyp0F1.apply(b, z).detach().item()
+        true_res = torch.cosh(2 * torch.sqrt(z)).detach().item()
 
-        self.assertAlmostEqual(res, torch.cosh(2 * torch.sqrt(z)))
+        self.assertAlmostEqual(res, true_res)
 
     def test_hyp0f1_der(self):
         z = torch.tensor(1.0, requires_grad=True)
         b = torch.tensor(0.5, requires_grad=False)
         res = Hyp0F1.apply(b, z)
 
-        dz = grad(res, z)[0]
+        dz = grad(res, z)[0].detach().item()
         true_dz = torch.sinh(2 * torch.sqrt(z)) / torch.sqrt(z)
 
-        self.assertAlmostEqual(dz, true_dz)
+        self.assertAlmostEqual(dz, true_dz.detach().item())
