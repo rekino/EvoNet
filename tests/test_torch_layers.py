f4aaaab77e66218a3ecee2f7f1ba4d51281a651c
ba <ba@rog.com>
2023-09-30 15:13:59 +0330

    add spherical linear layer

--- tests/test_torch_layers.py
+++ tests/test_torch_layers.py
@@ -1,4 +1,5 @@
 import unittest
+import torch
 
 from src.EvoNet.layers import SphericalLinear
 
@@ -8,3 +9,11 @@ class TestSphericalLinearLayer(unittest.TestCase):
     def test_constructor(self):
         layer = SphericalLinear(2, 3)
         self.assertIsInstance(layer, SphericalLinear)
+
+    def test_forward(self):
+        layer = SphericalLinear(2, 3)
+        x = torch.eye(2)
+        out = layer(x)
+
+        self.assertEquals(out.shape, (2, 4))
+        self.assertGreater(1, out[:, 1:].abs().max())
